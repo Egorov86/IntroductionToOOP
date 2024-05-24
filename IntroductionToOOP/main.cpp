7e7669e8423ed3257043212f8210358a357d377b
Egoro_st25@top-academy.ru <Egoro_st25@top-academy.ru>
2024-05-24 21:42:14 +0300

    IntroductionToOOP encoding

--- IntroductionToOOP/main.cpp
+++ IntroductionToOOP/main.cpp
@@ -1,6 +1,5 @@
 //IntroductionToOOP
 #include<iostream>
-#include <cmath> // для корня
 using namespace std;
 using std::cin;
 using std::cout;
@@ -30,20 +29,64 @@ public:
 	void set_y(double y)
 	{
 		this ->y = y;
-	}
-	/*double distance(const Point& AB) // пробовал метод 
+	}             
+	          //
+	Point()
 	{
-		return sqrt((B.get_x() - A.get_x()) * (B.get_x() - A.get_x()) + 
-		(B.get_y() - A.get_y()) * (B.get_y() - A.get_y()));
-	}*/
+		x = y = 0;
+		cout << "DefaultConstructor:\t" << this << endl;
+	}
+	Point(double x)
+	{
+		this->x = x;
+		this->y = 0;
+		cout << "1ArgConstructor:\t" << this << endl;
+	}
+	Point(double x, double y)
+	{
+		this->x = x;
+		this->y = y;
+		cout << "Constructor:\t\t" << this << endl;
+	}
+	Point(const Point& other)
+	{
+		this->x = other.x;
+		this->y = other.y;
+		cout << "CopyConstructor:\t" << this << endl;
+	}
+	~Point()
+	{
+		cout << "Destructor:\t\t" << this << endl;
+	}
+	          // Methods;
+
+	double distance( Point other) // пробовал метод 
+	{
+		//this - этот
+		//other - другой
+		//this - эта точка
+		//other - та точка
+
+		double x_distance = this->x - other.x;
+		double y_distance = this->y - other.y;
+		return sqrt(x_distance * x_distance + y_distance * y_distance);
+	}
+	void print()const
+	{
+		cout << "X = " << x << "\tY = " << y << endl;
+	}
 };
+
 double distance(const Point& A, const Point& B) // функция расстояние между точками
 {
-	return sqrt((B.get_x() - A.get_x()) * (B.get_x() - A.get_x()) +
-		(B.get_y() - A.get_y()) * (B.get_y() - A.get_y()));
+	double x_distantce = A.get_x() - B.get_x();
+	double y_distantce = A.get_y() - B.get_y();
+	return sqrt(x_distantce * x_distantce + y_distantce * y_distantce);
 }
 
 //#define STRUCT_POINT
+#define DISTANCE_CHECK
+//#define CONSTRUCTORS_CHECK
 
 void main()
 {
@@ -64,20 +107,43 @@ void main()
 	Point* pA = &A; // Pointer to 'A'
 	cout << pA->x << "\t" << pA->y << endl;
 #endif // STRUCT_POINT
+#ifdef DISTANCE_CHECK
+
 
 	Point A;
 	A.set_x(2);
 	A.set_y(3);
 	cout << A.get_x() << "\t" << A.get_y() << endl;
+
 	Point B;
-	B.set_x(3);
-	B.set_y(4);
+	B.set_x(7);
+	B.set_y(8);
 	cout << B.get_x() << "\t" << B.get_y() << endl;
 
-	double AB;
-	AB = distance(A, B);
-	cout << "Расстояние между точками A и B: " << AB << endl;
+	cout << "Расстояние от точки 'A' до точки 'B': " << A.distance(B) << endl;
+	cout << "Расстояние от точки 'B' до точки 'A': " << B.distance(A) << endl;
+	cout << "Расстояние между точками 'A' и 'B': " << distance(A, B) << endl;
+	cout << "Расстояние между точками 'B' и 'A': " << distance(B, A) << endl;
+
+#endif // DISTANCE_CHECK
+
+#ifdef CONSTRUCTORS_CHECK
+	Point A; // Default constructor
+	A.print();
+
+	Point B = 5;
+	B.print();
+
+	Point C(2, 3);
+	C.print();
+
+	Point D = C;
+	D.print();
+#endif // CONSTRUCTORS_CHECK
+
+
 }
+
 
 /*
 ______________________________________
